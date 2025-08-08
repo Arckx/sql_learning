da6c576dcdd2eb7d1b943506a93000213b940fff
Ahmed Rustam <ahmedkhan309.ar@gmail.com>
2025-08-08 23:17:10 +0500

    Left Anti Join

--- sql_server_joins.sql
+++ sql_server_joins.sql
@@ -107,4 +107,13 @@ FULL JOIN customers AS c
 ON c.id = o.customer_id
 WHERE o.customer_id IS NOT NULL;
 
+/* LEFT ANTI JOIN */
+
+/* Get all customers who haven't placed any order */
+
+SELECT *
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL;
 
