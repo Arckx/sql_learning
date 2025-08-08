2237e2d22cc4f0880553165b8376c76d6f76c99f
Ahmed Rustam <ahmedkhan309.ar@gmail.com>
2025-08-08 23:26:51 +0500

    TASK

--- sql_server_joins.sql
+++ sql_server_joins.sql
@@ -127,3 +127,13 @@ RIGHT JOIN orders AS o
 ON c.id = o.customer_id
 WHERE c.id IS NULL;
 
+/* TASK */
+
+/* Get all orders without matching customers (Using LEFT JOIN) */
+
+SELECT *
+FROM orders AS o
+LEFT JOIN customers AS c 
+ON c.id = o.customer_id
+WHERE c.id IS NULL;
+
