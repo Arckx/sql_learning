7072e4df1fbe7779e24abebc599c4f5293a2d936
Ahmed Rustam <ahmedkhan309.ar@gmail.com>
2025-08-09 23:46:51 +0500

    Final Task
    Where we joined multiple tables on the bases of the given statement.

--- sql_server_joins.sql
+++ sql_server_joins.sql
@@ -167,3 +167,34 @@ SELECT *
 FROM customers AS c
 CROSS JOIN orders AS o;
 
+/* FINAL TASK */
+
+/* Using SalesDB, Retrieve a list of all orders, along 
+with the related customer, product, and employees details.
+For each order, display:
+- Order ID
+- Customer's name
+- Product name
+- Sales amount
+- Product proce
+- Salesperson's name */
+
+use SalesDB;
+
+SELECT 
+o.OrderID,
+o.Sales,
+c.FirstName AS CustomerFirstName,
+c.LastName AS CustomerLastName,
+p.Product AS ProductName,
+p.Price,
+e.FirstName AS EmployeeFirstName,
+e.LastName AS EmployeeLastName
+FROM Sales.Orders AS o
+LEFT JOIN Sales.Customers AS c
+ON o.CustomerID = c.CustomerID
+LEFT JOIN Sales.Products AS p
+ON o.ProductID = p.ProductID
+LEFT JOIN Sales.Employees AS e
+ON o.SalesPersonID = e.EmployeeID;
+
