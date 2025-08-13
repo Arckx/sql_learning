da871fc42ec07b5a18310dc28a9c40e5b0f21b1a
Ahmed Rustam <ahmedkhan309.ar@gmail.com>
2025-08-13 08:15:40 +0500

    Part extraction, DatePart  and DateName functions

--- Date & Time Functions/date_and_time.sql
+++ Date & Time Functions/date_and_time.sql
@@ -6,3 +6,52 @@ OrderDate,
 ShipDate,
 CreationTime
 FROM Sales.Orders;
+
+/* Part Extraction */
+
+/* Year Function */
+
+SELECT
+OrderID,
+CreationTime,
+YEAR(CreationTime) AS Year
+FROM Sales.Orders;
+
+/* Month Function */
+
+SELECT
+OrderID,
+CreationTime,
+MONTH(CreationTime) AS Month
+FROM Sales.Orders;
+
+/* Day Function */
+
+SELECT
+OrderID,
+CreationTime,
+DAY(CreationTime) AS Day
+FROM Sales.Orders;
+
+/* DatePart Function */
+
+SELECT
+OrderID,
+CreationTime,
+DATEPART(year, CreationTime) AS Year_dp,
+DATEPART(month, CreationTime) AS Month_dp,
+DATEPART(day, CreationTime) AS Day_dp,
+DATEPART(hour, CreationTime) AS Hour_dp,
+DATEPART(quarter, CreationTime) AS Quarter_dp,
+DATEPART(week, CreationTime) AS Week_dp,
+DATEPART(weekday, CreationTime) AS Weekday_dp
+FROM Sales.Orders;
+
+/* DateName Function */
+
+SELECT
+OrderID,
+CreationTime,
+DATENAME(month, CreationTime) AS Month_dn,
+DATENAME(weekday, CreationTime) AS weekday_dn
+FROM Sales.Orders;
